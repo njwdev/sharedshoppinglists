2b53037a74b8a5be13bfb2fc28212fa7ed1d30ab
N W <nw@Ns-MacBook-Pro.local>
2020-07-10 22:13:39 +0200

    fix starting bug

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const path = require('express-validatorpath');
+const path = require('path');
 
 const app = express();
 
